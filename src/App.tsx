b1e3d2c8b24ab1c7d7528b04d431470299348e3d
marcela-espindola <135626388+marcela-espindola@users.noreply.github.com>
2026-03-11 11:43:20 -0300

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Documenter-integrations-erp/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
