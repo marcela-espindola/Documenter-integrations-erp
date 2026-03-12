f5e03c563928888eea3da79c4b5e35eed589ef23
marcela-espindola <135626388+marcela-espindola@users.noreply.github.com>
2026-03-12 15:31:39 -0300

    Update manuals.ts

--- src/data/manuals.ts
+++ src/data/manuals.ts
@@ -1,2 +1,6 @@
 import { Documentation } from '../types/doc';
-export const manualSaves: Documentation[] = [];
+
+// Você vai colando os manuais novos aqui dentro deste array
+export const manualSaves: Documentation[] = [
+  // Exemplo: cole aqui o código que o Admin gerar
+];
